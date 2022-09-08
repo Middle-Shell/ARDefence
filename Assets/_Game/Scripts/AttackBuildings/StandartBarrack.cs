20f2ae4b91ea148885f09870deb439f9c1f6dc47
Middle Shell <denob311302@gmail.com>
2022-09-08 22:04:18 +0300

    Bastille and Barracks
    Солдаты из барака нападают на бастилию

---
+++ Assets/_Game/Scripts/AttackBuildings/StandartBarrack.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using UnityEngine;
+using System.Collections.Generic;
+using System.Collections;
+using easyar;
+
+namespace _Game.Scripts.Enemies
+{
+    public class StandartBarrack : MonoBehaviour, IBaseBarrack
+    {
+        public  List<GameObject> _solders = new List<GameObject>();
+        [SerializeField] private float _perSecends = 1f;
+        [SerializeField] private GameObject _target;
+        [SerializeField] private GameObject _startPosition;// = Vector3.zero;
+
+        public void OnEnable()
+        {
+            Debug.Log("Awake");
+            //AddSolder(sol);
+            StartCoroutine(Spawn());
+        }
+
+        private void OnDisable()
+        {
+            StopAllCoroutines();
+        }
+
+        public void AddSolder(GameObject solder)
+        {
+            _solders.Add(solder);
+            Debug.Log("Solder added");
+        }
+
+        public void ExitSolder()
+        {
+            _solders.RemoveAt(0);
+            Debug.Log("Solder came out");
+        }
+        IEnumerator Spawn()
+        {
+            while (true)
+            {
+                Debug.Log("Start");
+                yield return new WaitForSeconds(_perSecends);
+                Debug.Log("time");
+                Debug.Log(_target.activeSelf);
+                if (_target.activeSelf)
+                {
+                    Debug.Log("Target");
+                    _solders[0].transform.position = _startPosition.transform.position;
+                    _solders[0].GetComponent<Enemy>().Target = _target.transform;
+                    _solders[0].GetComponent<Enemy>().Go();
+                    ExitSolder();
+                }
+            }
+        }
+    }
+}
---
+++ Assets/_Game/Scripts/Enemies/Enemy.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using UnityEngine;
+
+namespace easyar
+{
+    public class Enemy : UnityEngine.MonoBehaviour
+    {
+        [SerializeField] private Transform _target = null;
+        //[SerializeField] private float _speed = 2f;
+        
+        //когда они приходят на место или умирают, надо их возвращать, подумать насчёт event'ов
+        public Transform Target
+        {
+            set => _target = value;
+        }
+
+        public void Go()
+        {
+            StartCoroutine(Walk());
+        }
+
+        IEnumerator Walk()
+        {
+            while (true)
+            {
+                Debug.Log("Go");
+                yield return new WaitForSeconds(0.1f);
+                transform.position = Vector3.Lerp (transform.position, _target.position, 0.1f);
+            }
+        }
+    }
+}
