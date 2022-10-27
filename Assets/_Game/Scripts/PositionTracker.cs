9fbd4d29fb3ee0a1c05da4eeedc20601594f6dbd
Middle Shell <denob311302@gmail.com>
2022-10-27 11:43:45 +0300

    "Delete"ObjectOnTheCard

--- Assets/_Game/Scripts/PositionTracker.cs
+++ Assets/_Game/Scripts/PositionTracker.cs
@@ -1,6 +1,8 @@
 using System;
 using System.Collections;
 using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
 
 //[RequireComponent(typeof(ARRaycastManager))]
 public class PositionTracker : MonoBehaviour
@@ -8,6 +10,8 @@ public class PositionTracker : MonoBehaviour
     [SerializeField] private GameObject _plane;
     [SerializeField] private GameObject _selfPrefab;
     private Vector3 _oldPosition = Vector3.positiveInfinity;
+
+    public MeshRenderer mesh;
 
     void Start()
     {
@@ -18,22 +22,29 @@ public class PositionTracker : MonoBehaviour
     {
         while (true)
         {
-            print(_plane.name);
-            print("check");
+            if (this.transform.parent.gameObject.GetComponent<ARTrackedImage>().trackingState == TrackingState.Limited)
+            {
+                //делаем весь объект прозрачным
+                mesh.enabled = false;
+            }
+            else
+            {
+                mesh.enabled = true;
+            }
+            //print("check");
             if (Math.Abs(this.transform.position.x - _oldPosition.x) < 0.02 &&
                 Math.Abs(this.transform.position.z - _oldPosition.z) < 0.02)
-                //если объект не менял свою позицию больще чем на Х(0,12)(защита от случайной тряски)
+                //если объект не менял свою позицию больше чем на Х(0,02)(защита от случайной тряски)
                 //то установка в эту позицию(x, 0, z) префаба работающего объекта, с const y = 0
             {
-                print("Spawn");
+                
                 var inst = Instantiate(_selfPrefab,new Vector3(GetInstallPositionOnAxis(_oldPosition.x),
                     _plane.transform.position.y + 0.01f, 
                     GetInstallPositionOnAxis(_oldPosition.z)),
                     _plane.transform.rotation);
                 inst.gameObject.transform.SetParent(_plane.transform);
-                print(inst.transform.position);
-                StopAllCoroutines();//необходимо либо удалять этот объект, либо возобновлять работу корутины через N времени
-                //но точно нужно удалять объект прикрипленный к карте в случае, если он не трекается, что бы не висел в воздухе
+                //print(inst.transform.position);
+                yield return new WaitForSeconds(5f);
             }
                 
             yield return new WaitForSeconds(2f);//проверка на изменение позиции 2сек
