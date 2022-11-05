071ece530bc89418803935a290141d9ebc6a0750
Middle Shell <denob311302@gmail.com>
2022-11-05 14:49:44 +0300

    fix Freeze

--- Assets/_Game/Scripts/Services/PositionTracker.cs
+++ Assets/_Game/Scripts/Services/PositionTracker.cs
@@ -24,32 +24,31 @@ public class PositionTracker : MonoBehaviour
     
     IEnumerator Find()
     {
-            
-        //print("start check");
-        if (Math.Abs(this.transform.position.x - _oldPosition.x) < 0.02 &&
-            Math.Abs(this.transform.position.z - _oldPosition.z) < 0.02)
-            //если объект не менял свою позицию больше чем на Х(0,02)(защита от случайной тряски)
-            //то установка в эту позицию(x, 0, z) префаба работающего объекта, с const y = 0
+        while (true)
         {
-                
-            var inst = Instantiate(_selfPrefab,new Vector3(GetInstallPositionOnAxis(_oldPosition.x), 
-                _plane.transform.position.y + 0.01f, 
-                GetInstallPositionOnAxis(_oldPosition.z)), 
-                _plane.transform.rotation);
-            inst.gameObject.transform.SetParent(_plane.transform);
+            //print("start check");
+            if (Math.Abs(this.transform.position.x - _oldPosition.x) < 0.02 &&
+                Math.Abs(this.transform.position.z - _oldPosition.z) < 0.02)
+                //если объект не менял свою позицию больше чем на Х(0,02)(защита от случайной тряски)
+                //то установка в эту позицию(x, 0, z) префаба работающего объекта, с const y = 0
+            {
 
-            _isWorking = false;
-            SetInvisible(true);
-            //StopAllCoroutines();
-            yield return new WaitForSeconds(1f);
-            StartCoroutine(CheckState());
-            StopCoroutine(Find());
-            yield break;
+                var inst = Instantiate(_selfPrefab, new Vector3(GetInstallPositionOnAxis(_oldPosition.x),
+                        _plane.transform.position.y + 0.01f,
+                        GetInstallPositionOnAxis(_oldPosition.z)),
+                    _plane.transform.rotation);
+                inst.gameObject.transform.SetParent(_plane.transform);
+
+                
+                SetInvisible(true);
+                yield return new WaitForSeconds(1f);
+                StartCoroutine(CheckState());
+                yield break;
+            }
+
+            yield return new WaitForSeconds(2f); //проверка на изменение позиции 2сек
+            _oldPosition = this.transform.position;
         }
-           
-        yield return new WaitForSeconds(2f);//проверка на изменение позиции 2сек
-        _oldPosition = this.transform.position;
-        StartCoroutine(Find());
     }
 
     IEnumerator MaterialChanger()
@@ -61,7 +60,6 @@ public class PositionTracker : MonoBehaviour
             {
                 StartCoroutine(CheckState());
                 SetInvisible(true);
-                StopCoroutine(MaterialChanger());
                 yield break;
             }
             _Tx = this.transform.position.x;
@@ -74,6 +72,7 @@ public class PositionTracker : MonoBehaviour
                 {
                     try
                     {
+                        print(transform.position);
                         child.gameObject.GetComponent<MeshRenderer>().material = materials[0];
                         if (_Tx < 0 && _Tx > -0.25f) //сначала проверяем по Х потом уже по Z
                         {
@@ -106,6 +105,10 @@ public class PositionTracker : MonoBehaviour
                                     StartCoroutine(Find());
                                 }
                             }
+                        }
+                        else
+                        {
+                            StopCoroutine(Find());
                         }
                     }
                     catch
@@ -163,15 +166,18 @@ public class PositionTracker : MonoBehaviour
     }
     private IEnumerator CheckState()
     {
+        StopCoroutine(MaterialChanger());
         while (true)
         {
-            yield return new WaitForSeconds(.2f);
+            yield return new WaitForSeconds(0.2f);
             if (this.transform.parent.gameObject.GetComponent<ARTrackedImage>().trackingState
                 == TrackingState.Tracking)
             {
+                print("tracking");
                 SetInvisible(false);
+                _isWorking = false;
                 StartCoroutine(MaterialChanger());
-                StopCoroutine(CheckState());
+                yield break;
             }
         }
     }
