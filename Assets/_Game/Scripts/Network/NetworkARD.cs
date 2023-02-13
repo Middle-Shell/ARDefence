16598e9f25a5c5fa6cd9b1f4a460bd13c5547741
Dvvan696 <D.vvaann@gmail.com>
2023-02-13 22:54:37 +0300

    customNetwork

---
+++ Assets/_Game/Scripts/Network/NetworkARD.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+
+/*
+	Documentation: https://mirror-networking.gitbook.io/docs/components/network-manager
+	API Reference: https://mirror-networking.com/docs/api/Mirror.NetworkManager.html
+*/
+namespace Mirror
+{
+
+ 
+
+
+// Custom NetworkManager that simply assigns the correct racket positions when
+// spawning players. The built in RoundRobin spawn method wouldn't work after
+// someone reconnects (both players would be on the same side).
+    [AddComponentMenu("")]
+    public class NetworkARD : NetworkManager
+    {
+        public Transform MainSpawn;
+        public Transform DebilSpawn;
+
+
+        public override void OnServerAddPlayer(NetworkConnectionToClient conn)
+        {
+            // add player at correct spawn position
+            Transform start = numPlayers == 0 ? MainSpawn : DebilSpawn;
+            GameObject player = Instantiate(playerPrefab, start.position, start.rotation);
+            NetworkServer.AddPlayerForConnection(conn, player);
+
+
+        }
+
+        public override void OnServerDisconnect(NetworkConnectionToClient conn)
+        {
+
+            // call base functionality (actually destroys the player)
+            base.OnServerDisconnect(conn);
+        }
+
+    }
+}
